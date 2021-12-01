d1667ff3c32a4e62d59558973c6aa1ef3d541c28
AshreenS <shaikashreen@gmail.com>
2021-12-01 00:16:10 -0600

    login,registeradded

--- app.js
+++ app.js
@@ -3,29 +3,29 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var passport = require('passport'); 
-var LocalStrategy = require('passport-local').Strategy; 
 
-passport.use(new LocalStrategy( 
-  function(username, password, done) { 
-    Account.findOne({ username: username }, function (err, user) { 
-      if (err) { return done(err); } 
-      if (!user) { 
-        return done(null, false, { message: 'Incorrect username.' }); 
-      } 
-      if (!user.validPassword(password)) { 
-        return done(null, false, { message: 'Incorrect password.' }); 
-      } 
-      return done(null, user); 
-    }); 
-  } ));
- 
+var app = express();
 
-const connectionString =  process.env.MONGO_CON
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+passport.use(new LocalStrategy(
+  function (username, password, done) {
+    Account.findOne({ username: username }, function (err, user) {
+      if (err) { return done(err); }
+      if (!user) {
+        return done(null, false, { message: 'Incorrect username.' });
+      }
+      if (!user.validPassword(password)) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    });
+  }));
+
+const connectionString = process.env.MONGO_CON
 mongoose = require('mongoose');
-mongoose.connect(connectionString,{useNewUrlParser: true, useUnifiedTopology: true});
-
-
+mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true });
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -36,34 +36,31 @@ var Gucci = require("./models/gucci");
 var resourceRouter = require("./routes/resource");
 
 // We can seed the collection if needed on server start
-async function recreateDB(){
+async function recreateDB() {
   // Delete everything
   await Gucci.deleteMany();
   let instance1 = new
- Gucci({Itemname:"Belt", Quantity:50, price:100});
-  instance1.save( function(err,doc) {
-  if(err) return console.error(err);
-  console.log("First object saved")
+    Gucci({ Itemname: "Belt", Quantity: 50, price: 100 });
+  instance1.save(function (err, doc) {
+    if (err) return console.error(err);
+    console.log("First object saved")
   });
   let instance2 = new
-  Gucci({Itemname:"Heels", Quantity:20, price:79.99});
-  instance2.save( function(err,doc) {
-   if(err) return console.error(err);
-   console.log("Second object saved")
-   });
-   let instance3 = new
-   Gucci({Itemname:"Handbag", Quantity:30, price:45.88});
-    instance3.save( function(err,doc) {
-    if(err) return console.error(err);
+    Gucci({ Itemname: "Heels", Quantity: 20, price: 79.99 });
+  instance2.save(function (err, doc) {
+    if (err) return console.error(err);
+    console.log("Second object saved")
+  });
+  let instance3 = new
+    Gucci({ Itemname: "Handbag", Quantity: 30, price: 45.88 });
+  instance3.save(function (err, doc) {
+    if (err) return console.error(err);
     console.log("Third object saved")
-    });
- }
- let reseed = true;
- if (reseed) { recreateDB();}
+  });
+}
+let reseed = true;
+if (reseed) { recreateDB(); }
 
-
-
-var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -73,6 +70,13 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
@@ -82,22 +86,15 @@ app.use('/addmods', addmodsRouter);
 app.use('/selector', selectorRouter);
 app.use('/resource', resourceRouter);
 
-app.use(require('express-session')({ 
-  secret: 'keyboard cat', 
-  resave: false, 
-  saveUninitialized: false 
-})); 
-app.use(passport.initialize()); 
-app.use(passport.session()); 
 // passport config 
 // Use the existing connection 
 // The Account model  
-var Account =require('./models/account'); 
- 
-passport.use(new LocalStrategy(Account.authenticate())); 
-passport.serializeUser(Account.serializeUser()); 
-passport.deserializeUser(Account.deserializeUser()); 
- 
+var Account = require('./models/account');
+
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
+
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -119,9 +116,6 @@ app.use(function (err, req, res, next) {
 var db = mongoose.connection;
 //Bind connection to error event
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once("open", function(){console.log("Connection to DB succeeded")});
+db.once("open", function () { console.log("Connection to DB succeeded") });
 
 module.exports = app;
-
- 
-
